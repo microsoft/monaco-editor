35fffade6ac650bf953e7bbe043720654ab0b9db
Alex Dima <alexdima@microsoft.com>
2016-12-11 19:33:32 +0100

    Generate classic-style tokens

--- src/tokenization.ts
+++ src/tokenization.ts
@@ -13,15 +13,15 @@ export function createTokenizationSupport(supportComments: boolean): monaco.lang
     };
 }
 
-export const TOKEN_DELIM_OBJECT = 'punctuation.bracket.json';
-export const TOKEN_DELIM_ARRAY = 'punctuation.array.json';
-export const TOKEN_DELIM_COLON = 'punctuation.colon.json';
-export const TOKEN_DELIM_COMMA = 'punctuation.comma.json';
-export const TOKEN_VALUE_BOOLEAN = 'support.property-value.keyword.json';
-export const TOKEN_VALUE_NULL = 'support.property-value.constant.other.json';
-export const TOKEN_VALUE_STRING = 'support.property-value.string.value.json';
-export const TOKEN_VALUE_NUMBER = 'support.property-value.constant.numeric.json';
-export const TOKEN_PROPERTY_NAME = 'support.type.property-name.json';
+export const TOKEN_DELIM_OBJECT = 'delimiter.bracket.json';
+export const TOKEN_DELIM_ARRAY = 'delimiter.array.json';
+export const TOKEN_DELIM_COLON = 'delimiter.colon.json';
+export const TOKEN_DELIM_COMMA = 'delimiter.comma.json';
+export const TOKEN_VALUE_BOOLEAN = 'keyword.json';
+export const TOKEN_VALUE_NULL = 'keyword.json';
+export const TOKEN_VALUE_STRING = 'string.value.json';
+export const TOKEN_VALUE_NUMBER = 'number.json';
+export const TOKEN_PROPERTY_NAME = 'string.key.json';
 export const TOKEN_COMMENT_BLOCK = 'comment.block.json';
 export const TOKEN_COMMENT_LINE = 'comment.line.json';
 
