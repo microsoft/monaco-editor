4ecd94aff97f64601d1f9755e06726838e8b1d4a
Alex Dima <alexdima@microsoft.com>
2017-01-18 16:49:18 +0100

    Adjust module ids in tests to workaround r.js issue

--- test/all.js
+++ test/all.js
@@ -2,11 +2,14 @@ var requirejs = require("requirejs");
 
 requirejs.config({
     baseUrl: 'out',
+        paths: {
+        'vs/language/typescript': __dirname + '/../out'
+    },
     nodeRequire: require
 });
 
  requirejs([
-     'test/tokenization.test'
+     'vs/language/typescript/test/tokenization.test'
 ], function() {
     run(); // We can launch the tests!
 });
