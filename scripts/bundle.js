0944ea58972fa1007b1aa2a9fa1874273027bbd2
Alex Dima <alexdima@microsoft.com>
2018-03-12 16:07:41 +0100

    Add Copyright Header

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 const requirejs = require('requirejs');
 const path = require('path');
 const fs = require('fs');
