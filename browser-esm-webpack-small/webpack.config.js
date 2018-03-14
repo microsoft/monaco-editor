d3dce492681cac23fc379c82af86a7b7f063a74a
Alex Dima <alexdima@microsoft.com>
2018-03-14 17:00:41 +0100

    Add sample for small editor

---
+++ browser-esm-webpack-small/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+	mode: 'production',
+	entry: {
+		"app": './index.js',
+		"editor.worker": 'monaco-editor/esm/vs/editor/editor.worker.js',
+		// "json.worker": 'monaco-editor/esm/vs/language/json/json.worker',
+		// "css.worker": 'monaco-editor/esm/vs/language/css/css.worker',
+		// "html.worker": 'monaco-editor/esm/vs/language/html/html.worker',
+		// "ts.worker": 'monaco-editor/esm/vs/language/typescript/ts.worker',
+	},
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+		rules: [{
+			test: /\.css$/,
+			use: [ 'style-loader', 'css-loader' ]
+		}]
+	},
+	plugins: [
+		new webpack.IgnorePlugin(/^((fs)|(path)|(os)|(crypto)|(source-map-support))$/, /vs\/language\/typescript\/lib/),
+		new UglifyJSPlugin()
+	],
+};
