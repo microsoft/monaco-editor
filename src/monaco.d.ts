0b3e6b9b53e94715b166e61831468b3fd60d4e68
Alex Dima <alexdima@microsoft.com>
2018-03-15 10:49:51 +0100

    Avoid using `monaco` in `.d.ts` file

--- src/monaco.d.ts
+++ src/monaco.d.ts
@@ -147,6 +147,6 @@ declare module monaco.languages.typescript {
     export var typescriptDefaults: LanguageServiceDefaults;
     export var javascriptDefaults: LanguageServiceDefaults;
 
-    export var getTypeScriptWorker: () => monaco.Promise<any>;
-    export var getJavaScriptWorker: () => monaco.Promise<any>;
+    export var getTypeScriptWorker: () => Promise<any>;
+    export var getJavaScriptWorker: () => Promise<any>;
 }
