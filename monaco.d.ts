9589cfacb899d6be037d7173bdf6e3909e6d053e
Alex Dima <alexdima@microsoft.com>
2018-04-12 09:31:09 +0200

    Push .d.ts files

--- monaco.d.ts
+++ monaco.d.ts
@@ -1,6 +1,6 @@
 /*!-----------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
- * Type definitions for monaco-editor v0.11.1
+ * Type definitions for monaco-editor v0.12.0
  * Released under the MIT license
 *-----------------------------------------------------------*/
 /*---------------------------------------------------------------------------------------------
