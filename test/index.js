5f84834ffa16cec7b7ecba846ef7b4b219b6783f
Alex Dima <alexdima@microsoft.com>
2018-06-12 09:43:14 +0200

    Clarify example Add launch configuration

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 
-import * as monaco from 'monaco-editor';
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 
 monaco.editor.create(document.getElementById('container'), {
     value: 'console.log("Hello, world")',
