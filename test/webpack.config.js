50ced222cb50a43fa19621745b537a2e77889839
Mike Greiling <mike@pixelcog.com>
2018-07-03 01:19:41 -0500

    fix npm test script

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -4,6 +4,7 @@ const path = require('path');
 module.exports = {
     mode: 'development',
     entry: './index.js',
+    context: __dirname,
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'app.js'
