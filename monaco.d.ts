c36f474eb59c8f821d19b202e4f1f12ae35a1ec1
Alex Dima <alexdima@microsoft.com>
2018-08-11 00:11:50 +0200

    Update docs

--- monaco.d.ts
+++ monaco.d.ts
@@ -1,6 +1,6 @@
 /*!-----------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
- * Type definitions for monaco-editor v0.14.1
+ * Type definitions for monaco-editor v0.14.2
  * Released under the MIT license
 *-----------------------------------------------------------*/
 /*---------------------------------------------------------------------------------------------
