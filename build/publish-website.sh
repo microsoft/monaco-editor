d5857b5e0838e727f8cc2759b8f9108e0898f119
Alex Dima <alexdima@microsoft.com>
2018-11-09 17:20:54 +0100

    More tweaks

--- build/publish-website.sh
+++ build/publish-website.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-REMOTE_URL="$(git remote get-url origin)"
 COMMITTER_USER_NAME="$(git log --format='%an' -1)"
 COMMITTER_EMAIL="$(git log --format='%ae' -1)"
 
@@ -8,7 +7,7 @@ cd ../monaco-editor-website
 git init
 git config user.name "${COMMITTER_USER_NAME}"
 git config user.email "${COMMITTER_EMAIL}"
-git remote add origin "${REMOTE_URL}"
+git remote add origin "https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/Microsoft/monaco-editor.git"
 git checkout -b gh-pages
 git add .
 git commit -m "Publish website"
