9eb201f72feea38a85f66a3e9a765b0ea37d0751
Alex Dima <alexdima@microsoft.com>
2018-11-13 15:07:55 +0100

    Improve type definition

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,24 @@
-declare module 'monaco-editor-webpack-plugin';
+
+export interface IMonacoEditorWebpackPluginOpts {
+    /**
+     * custom output path for worker scripts, relative to the main webpack `output.path`.
+     * Defaults to ''.
+     */
+    output: string;
+
+    /**
+     * Include only a subset of the languages supported.
+     */
+    languages: string[];
+
+    /**
+     * Include only a subset of the editor features.
+     * Use e.g. '!contextmenu' to exclude a certain feature.
+     */
+    features: string[];
+}
+
+export default class MonacoEditorWebpackPlugin {
+    constructor(opts?: IMonacoEditorWebpackPluginOpts)
+}
+
