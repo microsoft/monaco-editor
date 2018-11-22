71728e7e92368f9b677aa9fa8d62f05fd4704e07
Tim Perry <pimterry@gmail.com>
2018-11-22 14:23:41 +0100

    Mark all options as optional in the type definitions

--- index.d.ts
+++ index.d.ts
@@ -4,18 +4,18 @@ export interface IMonacoEditorWebpackPluginOpts {
      * custom output path for worker scripts, relative to the main webpack `output.path`.
      * Defaults to ''.
      */
-    output: string;
+    output?: string;
 
     /**
      * Include only a subset of the languages supported.
      */
-    languages: string[];
+    languages?: string[];
 
     /**
      * Include only a subset of the editor features.
      * Use e.g. '!contextmenu' to exclude a certain feature.
      */
-    features: string[];
+    features?: string[];
 }
 
 export default class MonacoEditorWebpackPlugin {
