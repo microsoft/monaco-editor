c79ea537b514999054a899685e89e178d2c32e90
Alex Dima <alexdima@microsoft.com>
2019-08-14 16:05:10 +0200

    Simplify sample, update npm deps to remove npm security errors

--- browser-esm-webpack-monaco-plugin/index.js
+++ browser-esm-webpack-monaco-plugin/index.js
@@ -1,4 +1,4 @@
-import * as monaco from "monaco-editor";
+import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
 
 (function () {
 	// create div to avoid needing a HtmlWebpackPlugin template
--- browser-esm-webpack-monaco-plugin/webpack.config.js
+++ browser-esm-webpack-monaco-plugin/webpack.config.js
@@ -1,43 +1,22 @@
 const path = require("path");
 const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: process.env.NODE_ENV,
-    entry: "./index.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "[name].bundle.js",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                ],
-            },
-            {
-                test: /\.(png|jpg|gif|svg|woff2?|ttf|eot|otf)$/,
-                use: [
-                    {
-                        loader: "file-loader",
-                    },
-                ],
-            },
-        ],
-    },
-    plugins: [
-        new MonacoWebpackPlugin({
-            languages: ["typescript", "javascript", "css"],
-        }),
-		new HtmlWebpackPlugin()
-    ],
-    devServer: {
-        port: 4000,
-		hot: process.env.NODE_ENV === 'development',
-		open: true
-    },
-    devtool: process.argv.includes("--use-sourcemaps") ? "inline-source-map" : false,
+	mode: process.env.NODE_ENV,
+	entry: "./index.js",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "[name].bundle.js",
+	},
+	module: {
+		rules: [{
+			test: /\.css$/,
+			use: ["style-loader", "css-loader",],
+		}],
+	},
+	plugins: [
+		new MonacoWebpackPlugin({
+			languages: ["typescript", "javascript", "css"],
+		})
+	]
 };
