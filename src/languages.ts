f6223d69e6dab206b840387baf65ae1b311bc443
Alexandru Dima <alexdima@microsoft.com>
2019-12-19 10:41:50 +0100

    Remove worker:undefined

--- src/languages.ts
+++ src/languages.ts
@@ -4,42 +4,34 @@ const languagesArr: IFeatureDefinition[] = [
   {
     label: 'apex',
     entry: 'vs/basic-languages/apex/apex.contribution',
-    worker: undefined,
   },
   {
     label: 'azcli',
     entry: 'vs/basic-languages/azcli/azcli.contribution',
-    worker: undefined,
   },
   {
     label: 'bat',
     entry: 'vs/basic-languages/bat/bat.contribution',
-    worker: undefined,
   },
   {
     label: 'clojure',
     entry: 'vs/basic-languages/clojure/clojure.contribution',
-    worker: undefined,
   },
   {
     label: 'coffee',
     entry: 'vs/basic-languages/coffee/coffee.contribution',
-    worker: undefined,
   },
   {
     label: 'cpp',
     entry: 'vs/basic-languages/cpp/cpp.contribution',
-    worker: undefined,
   },
   {
     label: 'csharp',
     entry: 'vs/basic-languages/csharp/csharp.contribution',
-    worker: undefined,
   },
   {
     label: 'csp',
     entry: 'vs/basic-languages/csp/csp.contribution',
-    worker: undefined,
   },
   {
     label: 'css',
@@ -55,27 +47,22 @@ const languagesArr: IFeatureDefinition[] = [
   {
     label: 'dockerfile',
     entry: 'vs/basic-languages/dockerfile/dockerfile.contribution',
-    worker: undefined,
   },
   {
     label: 'fsharp',
     entry: 'vs/basic-languages/fsharp/fsharp.contribution',
-    worker: undefined,
   },
   {
     label: 'go',
     entry: 'vs/basic-languages/go/go.contribution',
-    worker: undefined,
   },
   {
     label: 'graphql',
     entry: 'vs/basic-languages/graphql/graphql.contribution',
-    worker: undefined,
   },
   {
     label: 'handlebars',
     entry: 'vs/basic-languages/handlebars/handlebars.contribution',
-    worker: undefined,
   },
   {
     label: 'html',
@@ -91,17 +78,14 @@ const languagesArr: IFeatureDefinition[] = [
   {
     label: 'ini',
     entry: 'vs/basic-languages/ini/ini.contribution',
-    worker: undefined,
   },
   {
     label: 'java',
     entry: 'vs/basic-languages/java/java.contribution',
-    worker: undefined,
   },
   {
     label: 'javascript',
     entry: 'vs/basic-languages/javascript/javascript.contribution',
-    worker: undefined,
   },
   {
     label: 'json',
@@ -114,142 +98,114 @@ const languagesArr: IFeatureDefinition[] = [
   {
     label: 'less',
     entry: 'vs/basic-languages/less/less.contribution',
-    worker: undefined,
   },
   {
     label: 'lua',
     entry: 'vs/basic-languages/lua/lua.contribution',
-    worker: undefined,
   },
   {
     label: 'markdown',
     entry: 'vs/basic-languages/markdown/markdown.contribution',
-    worker: undefined,
   },
   {
     label: 'msdax',
     entry: 'vs/basic-languages/msdax/msdax.contribution',
-    worker: undefined,
   },
   {
     label: 'mysql',
     entry: 'vs/basic-languages/mysql/mysql.contribution',
-    worker: undefined,
   },
   {
     label: 'objective',
     entry: 'vs/basic-languages/objective-c/objective-c.contribution',
-    worker: undefined,
   },
   {
     label: 'perl',
     entry: 'vs/basic-languages/perl/perl.contribution',
-    worker: undefined,
   },
   {
     label: 'pgsql',
     entry: 'vs/basic-languages/pgsql/pgsql.contribution',
-    worker: undefined,
   },
   {
     label: 'php',
     entry: 'vs/basic-languages/php/php.contribution',
-    worker: undefined,
   },
   {
     label: 'postiats',
     entry: 'vs/basic-languages/postiats/postiats.contribution',
-    worker: undefined,
   },
   {
     label: 'powerquery',
     entry: 'vs/basic-languages/powerquery/powerquery.contribution',
-    worker: undefined,
   },
   {
     label: 'powershell',
     entry: 'vs/basic-languages/powershell/powershell.contribution',
-    worker: undefined,
   },
   {
     label: 'pug',
     entry: 'vs/basic-languages/pug/pug.contribution',
-    worker: undefined,
   },
   {
     label: 'python',
     entry: 'vs/basic-languages/python/python.contribution',
-    worker: undefined,
   },
   {
     label: 'r',
     entry: 'vs/basic-languages/r/r.contribution',
-    worker: undefined,
   },
   {
     label: 'razor',
     entry: 'vs/basic-languages/razor/razor.contribution',
-    worker: undefined,
   },
   {
     label: 'redis',
     entry: 'vs/basic-languages/redis/redis.contribution',
-    worker: undefined,
   },
   {
     label: 'redshift',
     entry: 'vs/basic-languages/redshift/redshift.contribution',
-    worker: undefined,
   },
   {
     label: 'ruby',
     entry: 'vs/basic-languages/ruby/ruby.contribution',
-    worker: undefined,
   },
   {
     label: 'rust',
     entry: 'vs/basic-languages/rust/rust.contribution',
-    worker: undefined,
   },
   {
     label: 'sb',
     entry: 'vs/basic-languages/sb/sb.contribution',
-    worker: undefined,
   },
   {
     label: 'scheme',
     entry: 'vs/basic-languages/scheme/scheme.contribution',
-    worker: undefined,
   },
   {
     label: 'scss',
     entry: 'vs/basic-languages/scss/scss.contribution',
-    worker: undefined,
   },
   {
     label: 'shell',
     entry: 'vs/basic-languages/shell/shell.contribution',
-    worker: undefined,
   },
   {
     label: 'solidity',
     entry: 'vs/basic-languages/solidity/solidity.contribution',
-    worker: undefined,
   },
   {
     label: 'sql',
     entry: 'vs/basic-languages/sql/sql.contribution',
-    worker: undefined,
   },
   {
     label: 'st',
     entry: 'vs/basic-languages/st/st.contribution',
-    worker: undefined,
   },
   {
     label: 'swift',
     entry: 'vs/basic-languages/swift/swift.contribution',
-    worker: undefined,
   },
   {
     label: 'typescript',
@@ -265,17 +221,14 @@ const languagesArr: IFeatureDefinition[] = [
   {
     label: 'vb',
     entry: 'vs/basic-languages/vb/vb.contribution',
-    worker: undefined,
   },
   {
     label: 'xml',
     entry: 'vs/basic-languages/xml/xml.contribution',
-    worker: undefined,
   },
   {
     label: 'yaml',
     entry: 'vs/basic-languages/yaml/yaml.contribution',
-    worker: undefined,
   },
 ];
 
