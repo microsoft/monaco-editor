551115f82dfdd2b8b65bbc49b105a7a13f783542
Alex Dima <alexdima@microsoft.com>
2020-02-10 18:10:55 +0100

    Fix issue where test page does not run in IE

--- test/dev-setup.js
+++ test/dev-setup.js
@@ -152,7 +152,7 @@
 	self.getCodiconPath = function(PATH_PREFIX) {
 		PATH_PREFIX = PATH_PREFIX || '';
 		const result = RESOLVED_CORE.getResolvedPath(PATH_PREFIX);
-		return `${result}/base/browser/ui/codiconLabel/codicon/codicon.ttf`;
+		return result + '/base/browser/ui/codiconLabel/codicon/codicon.ttf';
 	};
 
 
