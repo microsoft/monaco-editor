9d8859a82deb56d020213892707e1a7ebc7985f7
Hugo Fonseca <1098293+fonsecas72@users.noreply.github.com>
2020-09-02 23:38:01 +0100

    Improving terraform hcl

--- src/hcl/hcl.contribution.ts
+++ src/hcl/hcl.contribution.ts
@@ -7,8 +7,8 @@
 import { registerLanguage } from '../_.contribution';
 
 registerLanguage({
-	id: 'hcl',
-	extensions: ['.tf', '.tfvars', '.hcl'],
-	aliases: ['Terraform', 'tf', 'HCL', 'hcl'],
-	loader: () => import('./hcl')
+    id: 'hcl',
+    extensions: ['.tf', '.tfvars', '.hcl'],
+    aliases: ['Terraform', 'tf', 'HCL', 'hcl'],
+    loader: () => import('./hcl')
 });
