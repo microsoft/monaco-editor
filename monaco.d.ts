2f06141c30657cd8e68fa9f74178e6030401eac3
Alex Dima <alexdima@microsoft.com>
2020-09-07 15:56:34 +0200

    update to latest JSON language service

--- monaco.d.ts
+++ monaco.d.ts
@@ -3,6 +3,8 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+/// <reference path="node_modules/monaco-editor-core/monaco.d.ts" />
+
 declare namespace monaco.languages.json {
 	export interface DiagnosticsOptions {
 		/**
--- scripts/dts.js
+++ scripts/dts.js
@@ -20,6 +20,8 @@ let result = [
 	` *  Licensed under the MIT License. See License.txt in the project root for license information.`,
 	` *--------------------------------------------------------------------------------------------*/`,
 	``,
+	`/// <reference path="node_modules/monaco-editor-core/monaco.d.ts" />`,
+	``,
 	`declare namespace monaco.languages.json {`
 ];
 for (let line of lines) {
