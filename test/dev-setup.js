1ba6bff6f70e03f3ed9c90a6aab0b3e525589849
Alex Dima <alexdima@microsoft.com>
2020-09-19 02:35:37 +0200

    update codicons path

--- test/dev-setup.js
+++ test/dev-setup.js
@@ -153,7 +153,7 @@
 	self.getCodiconPath = function(PATH_PREFIX) {
 		PATH_PREFIX = PATH_PREFIX || '';
 		const result = RESOLVED_CORE.getResolvedPath(PATH_PREFIX);
-		return result + '/base/browser/ui/codiconLabel/codicon/codicon.ttf';
+		return result + '/base/browser/ui/codicons/codicon/codicon.ttf';
 	};
 
 
