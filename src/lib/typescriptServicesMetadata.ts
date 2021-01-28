eaa07c08ddd4177c4a07f846e9eed2b9a77d7d50
Alexandru Dima <alexdima@microsoft.com>
2021-01-29 00:21:25 +0100

    Upgrade to typescript 4.1.3

--- src/lib/typescriptServicesMetadata.ts
+++ src/lib/typescriptServicesMetadata.ts
@@ -2,4 +2,4 @@
 // **NOTE**: Do not edit directly! This file is generated using `npm run import-typescript`
 //
 
-export const typescriptVersion = "4.0.3";
+export const typescriptVersion = "4.1.3";
