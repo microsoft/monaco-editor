ee12401f93762ea8213bb9feb3a03df72d0f4ee8
Jonatan Kłosko <jonatanklosko@gmail.com>
2021-04-09 18:05:42 +0200

    Add Elixir language definition and tokenizer

---
+++ src/elixir/elixir.contribution.ts
@@ -0,0 +1,13 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { registerLanguage } from '../_.contribution';
+
+registerLanguage({
+	id: 'elixir',
+	extensions: ['.ex', '.exs'],
+	aliases: ['Elixir', 'elixir', 'ex'],
+	loader: () => import('./elixir')
+});
