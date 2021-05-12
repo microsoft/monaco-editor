ae9e867dc6fba74ece589430fbbd4dc719735b81
Martin Aeschlimann <martinae@microsoft.com>
2021-05-12 18:31:35 +0200

    update monaco.d.ts

--- monaco.d.ts
+++ monaco.d.ts
@@ -8,11 +8,13 @@
 declare namespace monaco.languages.json {
 	export interface DiagnosticsOptions {
 		/**
-		 * If set, the validator will be enabled and perform syntax validation as well as schema based validation.
+		 * If set, the validator will be enabled and perform syntax and validation schema based validation,
+		 * unless `DiagnosticsOptions.schemaValidation` is set to `ignore`.
 		 */
 		readonly validate?: boolean;
 		/**
 		 * If set, comments are tolerated. If set to false, syntax errors will be emitted for comments.
+		 * `DiagnosticsOptions.allowComments` will override this setting.
 		 */
 		readonly allowComments?: boolean;
 		/**
@@ -24,7 +26,10 @@ declare namespace monaco.languages.json {
 			 */
 			readonly uri: string;
 			/**
-			 * A list of file names that are associated to the schema. The '*' wildcard can be used. For example '*.schema.json', 'package.json'
+			 * A list of glob patterns that describe for which file URIs the JSON schema will be used.
+			 * '*' and '**' wildcards are supported. Exclusion patterns start with '!'.
+			 * For example '*.schema.json', 'package.json', '!foo*.schema.json', 'foo/**\/BADRESP.json'.
+			 * A match succeeds when there is at least one pattern matching and last matching pattern does not start with '!'.
 			 */
 			readonly fileMatch?: string[];
 			/**
@@ -44,6 +49,14 @@ declare namespace monaco.languages.json {
 		 * The severity of problems that occurred when resolving and loading schemas. If set to 'ignore', schema resolving problems are not reported. If not set, 'warning' is used.
 		 */
 		readonly schemaRequest?: SeverityLevel;
+		/**
+		 * The severity of reported trailing commas. If not set, trailing commas will be reported as errors.
+		 */
+		readonly trailingCommas?: SeverityLevel;
+		/**
+		 * The severity of reported comments. If not set, 'DiagnosticsOptions.allowComments' defines whether comments are ignored or reported as errors.
+		 */
+		readonly comments?: SeverityLevel;
 	}
 	export type SeverityLevel = 'error' | 'warning' | 'ignore';
 	export interface ModeConfiguration {
