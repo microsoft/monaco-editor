fb643d08a48bfd1a5a45d0d2224ac302707a6f22
Sebastian Pahnke <pahnke.sebastian@gmail.com>
2021-06-01 10:08:27 +0200

    [JS/TS] Add support for the RegExp Match Indices flag

--- src/typescript/typescript.ts
+++ src/typescript/typescript.ts
@@ -239,7 +239,7 @@ export const language = {
 
 			// regular expression: ensure it is terminated before beginning (otherwise it is an opeator)
 			[
-				/\/(?=([^\\\/]|\\.)+\/([gimsuy]*)(\s*)(\.|;|,|\)|\]|\}|$))/,
+				/\/(?=([^\\\/]|\\.)+\/([dgimsuy]*)(\s*)(\.|;|,|\)|\]|\}|$))/,
 				{ token: 'regexp', bracket: '@open', next: '@regexp' }
 			],
 
@@ -312,7 +312,7 @@ export const language = {
 			[/@regexpesc/, 'regexp.escape'],
 			[/\\\./, 'regexp.invalid'],
 			[
-				/(\/)([gimsuy]*)/,
+				/(\/)([dgimsuy]*)/,
 				[{ token: 'regexp', bracket: '@close', next: '@pop' }, 'keyword.other']
 			]
 		],
