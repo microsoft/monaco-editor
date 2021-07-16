34888a7d3e6561c824faa7d75760f788041896e1
Henning Dieterichs <hdieterichs@microsoft.com>
2021-07-16 10:59:20 +0200

    Downgrades monaco-json to 3.5.0 to fix #2578.

--- typedoc/monaco.d.ts
+++ typedoc/monaco.d.ts
@@ -7413,13 +7413,11 @@ declare namespace monaco.languages.css {
 declare namespace monaco.languages.json {
     export interface DiagnosticsOptions {
         /**
-         * If set, the validator will be enabled and perform syntax and schema based validation,
-         * unless `DiagnosticsOptions.schemaValidation` is set to `ignore`.
+         * If set, the validator will be enabled and perform syntax validation as well as schema based validation.
          */
         readonly validate?: boolean;
         /**
          * If set, comments are tolerated. If set to false, syntax errors will be emitted for comments.
-         * `DiagnosticsOptions.allowComments` will override this setting.
          */
         readonly allowComments?: boolean;
         /**
@@ -7431,10 +7429,7 @@ declare namespace monaco.languages.json {
              */
             readonly uri: string;
             /**
-             * A list of glob patterns that describe for which file URIs the JSON schema will be used.
-             * '*' and '**' wildcards are supported. Exclusion patterns start with '!'.
-             * For example '*.schema.json', 'package.json', '!foo*.schema.json', 'foo/**\/BADRESP.json'.
-             * A match succeeds when there is at least one pattern matching and last matching pattern does not start with '!'.
+             * A list of file names that are associated to the schema. The '*' wildcard can be used. For example '*.schema.json', 'package.json'
              */
             readonly fileMatch?: string[];
             /**
@@ -7454,14 +7449,6 @@ declare namespace monaco.languages.json {
          * The severity of problems that occurred when resolving and loading schemas. If set to 'ignore', schema resolving problems are not reported. If not set, 'warning' is used.
          */
         readonly schemaRequest?: SeverityLevel;
-        /**
-         * The severity of reported trailing commas. If not set, trailing commas will be reported as errors.
-         */
-        readonly trailingCommas?: SeverityLevel;
-        /**
-         * The severity of reported comments. If not set, 'DiagnosticsOptions.allowComments' defines whether comments are ignored or reported as errors.
-         */
-        readonly comments?: SeverityLevel;
     }
     export type SeverityLevel = 'error' | 'warning' | 'ignore';
     export interface ModeConfiguration {
