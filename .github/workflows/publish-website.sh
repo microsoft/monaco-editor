04742ae492beb4776dcdeb8fb452fd4be9e5a219
Alex Dima <alexdima@microsoft.com>
2021-11-16 23:57:49 +0100

    Try to use `x-access-token`

--- .github/workflows/publish-website.sh
+++ .github/workflows/publish-website.sh
@@ -3,14 +3,12 @@
 COMMITTER_USER_NAME="$(git log --format='%an' -1)"
 COMMITTER_EMAIL="$(git log --format='%ae' -1)"
 
-git remote -v
-
 cd ../monaco-editor-website
 git init
 git config user.name "${COMMITTER_USER_NAME}"
 git config user.email "${COMMITTER_EMAIL}"
-# git remote add origin "https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/microsoft/monaco-editor.git"
-# git checkout -b gh-pages
-# git add .
-# git commit -m "Publish website"
-# git push origin gh-pages --force
+git remote add origin "https://x-access-token:${GITHUB_TOKEN}@github.com/microsoft/monaco-editor.git"
+git checkout -b gh-pages
+git add .
+git commit -m "Publish website"
+git push origin gh-pages --force
