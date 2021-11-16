ed1ee25b7bc377a727e7dbcb9a59c6a979e8e98c
Alex Dima <alexdima@microsoft.com>
2021-11-16 09:23:03 +0100

    Update all dependencies

---
+++ build/npm/removeAll.js
@@ -0,0 +1,23 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+//@ts-check
+
+const glob = require('glob');
+const path = require('path');
+const fs = require('fs');
+
+const REPO_ROOT = path.join(__dirname, '../../');
+
+const files = glob.sync('**/package-lock.json', {
+	cwd: REPO_ROOT,
+	ignore: ['**/node_modules/**', '**/out/**', '**/release/**']
+});
+
+for (const file of files) {
+	const filePath = path.join(REPO_ROOT, file);
+	console.log(`Deleting ${file}...`);
+	fs.unlinkSync(filePath);
+}
