33ea399f99d8c36a0dda62f86c0a71978456459a
Alex Dima <alexdima@microsoft.com>
2021-11-16 22:33:13 +0100

    Remove unused typedoc theme

--- monaco-editor/typedoc/theme/assets/js/src/typedoc/utils/debounce.ts
+++
@@ -1,7 +0,0 @@
-export const debounce = (fn: Function, wait: number = 100) => {
-    let timeout: ReturnType<typeof setTimeout>;
-    return (...args: any[]) => {
-      clearTimeout(timeout)
-      timeout = setTimeout(() => fn(args), wait)
-    }
-}
