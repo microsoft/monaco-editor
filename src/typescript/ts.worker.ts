2d6b5590c7bf2283382adf32a0bce08344bdbad1
Alex Dima <alexdima@microsoft.com>
2021-11-17 15:17:14 +0100

    Remove unnecessary `"use strict"` directives

--- src/typescript/ts.worker.ts
+++ src/typescript/ts.worker.ts
@@ -2,7 +2,6 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-'use strict';
 
 import * as edworker from 'monaco-editor-core/esm/vs/editor/editor.worker';
 import { TypeScriptWorker, ICreateData } from './tsWorker';
