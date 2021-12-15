3f47c32190f594001e692d6109c1db8c1a60d664
Ryan Graff <ryantgraff@resistdesign.com>
2021-12-14 20:32:36 -0500

    doc: (browser-esm-parcel) Simplify build

--- samples/browser-esm-parcel/src/index.js
+++ samples/browser-esm-parcel/src/index.js
@@ -1,20 +1,25 @@
+import JSONWorker from 'url:monaco-editor/esm/vs/language/json/json.worker.js';
+import CSSWorker from 'url:monaco-editor/esm/vs/language/css/css.worker.js';
+import HTMLWorker from 'url:monaco-editor/esm/vs/language/html/html.worker.js';
+import TSWorker from 'url:monaco-editor/esm/vs/language/typescript/ts.worker.js';
+import EditorWorker from 'url:monaco-editor/esm/vs/editor/editor.worker.js';
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.main.js';
 
 self.MonacoEnvironment = {
 	getWorkerUrl: function (moduleId, label) {
 		if (label === 'json') {
-			return './json.worker.js';
+			return JSONWorker;
 		}
 		if (label === 'css' || label === 'scss' || label === 'less') {
-			return './css.worker.js';
+			return CSSWorker;
 		}
 		if (label === 'html' || label === 'handlebars' || label === 'razor') {
-			return './html.worker.js';
+			return HTMLWorker;
 		}
 		if (label === 'typescript' || label === 'javascript') {
-			return './ts.worker.js';
+			return TSWorker;
 		}
-		return './editor.worker.js';
+		return EditorWorker;
 	}
 };
 
