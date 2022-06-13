e20f9eaaf222428f15ddd7310a8e3f903dc07be7
junyiz <junyime@qq.com>
2022-06-13 09:29:23 +0800

    prettier code

--- samples/browser-script-vite-vue/src/App.vue
+++ samples/browser-script-vite-vue/src/App.vue
@@ -1,17 +1,17 @@
 <script setup>
-import Editor from './components/Editor.vue'
+import Editor from './components/Editor.vue';
 </script>
 
 <template>
-  <editor />
+	<editor />
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
