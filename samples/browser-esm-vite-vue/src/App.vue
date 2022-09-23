49719780cf65d56e7bfc15aa427842fcd7083a5b
gaofeixiang <gaofeixiang@yuanfudao.com>
2022-09-24 03:40:21 +0800

    excute code prettier

--- samples/browser-esm-vite-vue/src/App.vue
+++ samples/browser-esm-vite-vue/src/App.vue
@@ -1,26 +1,26 @@
 <script setup lang="ts">
-import { onMounted, onUnmounted, ref } from "vue";
-import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
+import { onMounted, onUnmounted, ref } from 'vue';
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 
 const editorItemRef = ref(null);
 let editorInstance: monaco.editor.IStandaloneCodeEditor;
 
 onMounted(() => {
-  if (editorItemRef.value && !editorInstance) {
-    monaco.editor.create(editorItemRef.value, {
-					value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
-					language: 'typescript'
-    });
-  }
+	if (editorItemRef.value && !editorInstance) {
+		monaco.editor.create(editorItemRef.value, {
+			value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
+			language: 'typescript'
+		});
+	}
 });
 
 onUnmounted(() => {
-  editorInstance?.dispose();
+	editorInstance?.dispose();
 });
 </script>
 
 <template>
-  <div class="editor" ref="editorItemRef"></div>
+	<div class="editor" ref="editorItemRef"></div>
 </template>
 
 <style lang="css" scoped>
--- samples/browser-esm-vite-vue/src/main.tsx
+++ samples/browser-esm-vite-vue/src/main.tsx
@@ -1,5 +1,5 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue';
+import App from './App.vue';
 import './userWorker';
 
-createApp(App).mount("#app")
+createApp(App).mount('#app');
