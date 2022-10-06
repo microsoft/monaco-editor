47bee85d4972dd0f65996f53435485d2c42bb762
Bruce Song <recall4056@gmail.com>
2022-10-07 00:15:09 +0800

    Add vue3 sample.

---
+++ samples/browser-esm-vite-vue3/src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import Editor from "./components/Editor.vue"
+</script>
+
+<template>
+  <Editor/>
+</template>
+
---
+++ samples/browser-esm-vite-vue3/src/components/Editor.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import * as monaco from 'monaco-editor';
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
+import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
+import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
+import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
+import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
+
+const editorRef = ref<HTMLElement>();
+
+// @ts-ignore
+self.MonacoEnvironment = {
+	getWorker(_: any, label: string) {
+		if (label === 'json') {
+			return new jsonWorker();
+		}
+		if (label === 'css' || label === 'scss' || label === 'less') {
+			return new cssWorker();
+		}
+		if (label === 'html' || label === 'handlebars' || label === 'razor') {
+			return new htmlWorker();
+		}
+		if (label === 'typescript' || label === 'javascript') {
+			return new tsWorker();
+		}
+		return new editorWorker();
+	}
+};
+
+onMounted(() => {
+	monaco.editor.create(editorRef.value as HTMLElement, {
+		value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
+		language: 'typescript'
+	});
+	//monaco.languages.typescript.typescriptDefaults.setEagerModelSync(true);
+});
+</script>
+
+<template>
+	<div ref="editorRef" class="editor" />
+</template>
+
+<style>
+.editor {
+	width: 100vw;
+	height: 100vh;
+}
+</style>
