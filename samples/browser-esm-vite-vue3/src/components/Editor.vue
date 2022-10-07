5e54656b7bcd6ea3341299d6a17a9802eb632dd5
Bruce Song <recall4056@gmail.com>
2022-10-07 10:23:52 +0800

    Improve.

--- samples/browser-esm-vite-vue3/src/components/Editor.vue
+++ samples/browser-esm-vite-vue3/src/components/Editor.vue
@@ -1,39 +1,21 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { onMounted, onUnmounted, ref } from 'vue';
 import * as monaco from 'monaco-editor';
-import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
-import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
-import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
-import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
-import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
 
 const editorRef = ref<HTMLElement>();
-
-// @ts-ignore
-self.MonacoEnvironment = {
-	getWorker(_: any, label: string) {
-		if (label === 'json') {
-			return new jsonWorker();
-		}
-		if (label === 'css' || label === 'scss' || label === 'less') {
-			return new cssWorker();
-		}
-		if (label === 'html' || label === 'handlebars' || label === 'razor') {
-			return new htmlWorker();
-		}
-		if (label === 'typescript' || label === 'javascript') {
-			return new tsWorker();
-		}
-		return new editorWorker();
-	}
-};
+const editorInstance = ref<monaco.editor.IStandaloneCodeEditor | null>(null);
 
 onMounted(() => {
-	monaco.editor.create(editorRef.value as HTMLElement, {
-		value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
-		language: 'typescript'
-	});
-	//monaco.languages.typescript.typescriptDefaults.setEagerModelSync(true);
+	if (editorRef.value && !editorInstance.value) {
+		editorInstance.value = monaco.editor.create(editorRef.value, {
+			value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
+			language: 'typescript'
+		});
+	}
+});
+
+onUnmounted(() => {
+	editorInstance.value?.dispose();
 });
 </script>
 
--- samples/browser-esm-vite-vue3/src/main.ts
+++ samples/browser-esm-vite-vue3/src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import './useWorker';
 
 createApp(App).mount('#app');
