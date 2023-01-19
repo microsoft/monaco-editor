64e21cec937c160bc47c6791a2c11888ff65d626
sunyuntian <ununian@outlook.com>
2023-01-20 00:08:06 +0800

    fix code format

--- website/playground/new-samples/extending-language-services/inlay-hints-provider-example/sample.js
+++ website/playground/new-samples/extending-language-services/inlay-hints-provider-example/sample.js
@@ -5,43 +5,43 @@ const result = f(2, 5);
 `;
 
 const editor = monaco.editor.create(document.getElementById('container'), {
-    value,
-    language: 'javascript'
+	value,
+	language: 'javascript'
 });
 
 monaco.languages.registerInlayHintsProvider('javascript', {
-    async provideInlayHints(model, range, token) {
-        return {
-            hints: [
-                {
-                    kind: monaco.languages.InlayHintKind.Type,
-                    position: { column: 13, lineNumber: 4 },
-                    label: `: Number`
-                },
-                {
-                    kind: monaco.languages.InlayHintKind.Type,
-                    position: { column: 13, lineNumber: 2 },
-                    label: `: Number`
-                },
-                {
-                    kind: monaco.languages.InlayHintKind.Type,
-                    position: { column: 16, lineNumber: 2 },
-                    label: `: Number`,
-                    paddingLeft: true // see difference between a and b parameter
-                },
-                {
-                    kind: monaco.languages.InlayHintKind.Parameter,
-                    position: { column: 18, lineNumber: 4 },
-                    label: `a:`
-                },
-                {
-                    kind: monaco.languages.InlayHintKind.Parameter,
-                    position: { column: 21, lineNumber: 4 },
-                    label: `b:`,
-                    paddingRight: true // similar to whitespaceBefore
-                }
-            ],
-            dispose: () => { }
-        }
-    }
+	async provideInlayHints(model, range, token) {
+		return {
+			hints: [
+				{
+					kind: monaco.languages.InlayHintKind.Type,
+					position: { column: 13, lineNumber: 4 },
+					label: `: Number`
+				},
+				{
+					kind: monaco.languages.InlayHintKind.Type,
+					position: { column: 13, lineNumber: 2 },
+					label: `: Number`
+				},
+				{
+					kind: monaco.languages.InlayHintKind.Type,
+					position: { column: 16, lineNumber: 2 },
+					label: `: Number`,
+					paddingLeft: true // see difference between a and b parameter
+				},
+				{
+					kind: monaco.languages.InlayHintKind.Parameter,
+					position: { column: 18, lineNumber: 4 },
+					label: `a:`
+				},
+				{
+					kind: monaco.languages.InlayHintKind.Parameter,
+					position: { column: 21, lineNumber: 4 },
+					label: `b:`,
+					paddingRight: true // similar to whitespaceBefore
+				}
+			],
+			dispose: () => {}
+		};
+	}
 });
