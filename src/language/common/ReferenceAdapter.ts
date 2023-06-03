5a6f457a7691f7d2b5ab6b4d2f72f762fbcc8ceb
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-06-03 17:45:39 -0300

    chore: add copyright notes on new files

--- src/language/common/ReferenceAdapter.ts
+++ src/language/common/ReferenceAdapter.ts
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 import * as lsTypes from 'vscode-languageserver-types';
 import { languages, editor, Position, CancellationToken } from '../../fillers/monaco-editor-core';
 import { fromPosition } from './CompletionAdapter';
