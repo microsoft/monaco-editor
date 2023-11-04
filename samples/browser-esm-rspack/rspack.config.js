03ef28f134c4c3021f19de05d4836600fbe719cb
SyMind <dacongsama@live.com>
2023-11-05 00:56:26 +0800

    chore: add browser-esm-rspack sample

---
+++ samples/browser-esm-rspack/rspack.config.js
@@ -0,0 +1,23 @@
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+
+/**
+ * @type {import('@rspack/cli').Configuration}
+ */
+module.exports = {
+	context: __dirname,
+	entry: {
+		main: "./index.js"
+	},
+	builtins: {
+		html: [
+			{
+				template: "./index.html"
+			}
+		]
+	},
+	plugins: [
+		new MonacoWebpackPlugin({
+			languages: ['typescript', 'javascript', 'css']
+		})
+	]
+};
